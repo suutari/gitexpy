c4a6912d3f81b84e9ba50ef44cada3c90e1f6614
Tuomas Suutari <tuomas.suutari@gmail.com>
2013-10-17 13:09:08 +0300

    parse_zlib_deflate_stream: Allow filename argument

--- gitexpy/parse_zlib_deflate_stream.py
+++ gitexpy/parse_zlib_deflate_stream.py
@@ -1,9 +1,12 @@
 from . import zlibstream
 
 def main(sys):
+    stream = sys.stdin
     do_profile = False
     if len(sys.argv) > 1 and sys.argv[1] == '--profile':
         do_profile = True
+    elif len(sys.argv) > 1:
+        stream = open(sys.argv[1], 'rb')
     if do_profile:
         import cProfile
         import pstats
@@ -18,7 +21,7 @@ def main(sys):
         stats.sort_stats('cumulative')
         stats.print_stats()
     else:
-        zlibstream.parse()
+        zlibstream.parse(stream)
 
 if __name__ == '__main__':
     import sys
